f53be6688cdfe22f8bf20826934b399f65f8c6ca
Chris Fung <chris.fung2@gmail.com>
2020-01-12 00:12:05 -0800

    database searching

---
+++ app/src/main/java/com/example/rl_mii_plaza/Face/NoURLFoundException.java
@@ -0,0 +1,4 @@
+package com.example.rl_mii_plaza.Face;
+
+public class NoURLFoundException extends Exception {
+}
---
+++ app/src/main/java/com/example/rl_mii_plaza/Face/NoUserFoundException.java
@@ -0,0 +1,4 @@
+package com.example.rl_mii_plaza.Face;
+
+public class NoUserFoundException extends Exception {
+}
