e1c167e96b474f0c83d54ad099ccf9225e0b36b0
Chris Fung <chris.fung2@gmail.com>
2020-01-11 18:57:41 -0800

    working on making face lists

---
+++ app/src/test/java/com/example/rl_mii_plaza/FaceTest.java
@@ -0,0 +1,13 @@
+package com.example.rl_mii_plaza;
+
+import com.microsoft.projectoxford.face.contract.FaceList;
+
+import org.junit.Test;
+
+public class FaceTest {
+
+    @Test
+    public void faceListTest() {
+        FaceList facelist = new FaceList();
+    }
+}
